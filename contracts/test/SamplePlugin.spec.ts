6135abf999f033f72ece7e27550bd9e9a3677987
Richard Meissner <rmeissner@users.noreply.github.com>
2023-07-13 20:15:28 +0200

    Fix formatting and enforce linting in workflow (#11)

--- contracts/test/SamplePlugin.spec.ts
+++ contracts/test/SamplePlugin.spec.ts
@@ -1,7 +1,7 @@
 import hre, { deployments } from "hardhat";
 import { expect } from "chai";
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
-import {  getSamplePlugin } from "./utils/contracts";
+import { getSamplePlugin } from "./utils/contracts";
 import { loadPluginMetaData } from "./utils/metadata";
 
 describe("SamplePlugin", async () => {
@@ -15,25 +15,26 @@ describe("SamplePlugin", async () => {
         await deployments.fixture();
         const plugin = await getSamplePlugin();
         return {
-            plugin
-        }
+            plugin,
+        };
     });
 
     it("should be inititalized correctly", async () => {
-        const { plugin } = await setup()
+        const { plugin } = await setup();
+        console.log(user1);
         expect(await plugin.name()).to.be.eq("Sample Plugin");
         expect(await plugin.version()).to.be.eq("1.0.0");
         expect(await plugin.requiresRootAccess()).to.be.false;
     });
 
     it("can retrieve meta data for module", async () => {
-        const { plugin } = await setup()
+        const { plugin } = await setup();
         expect(await loadPluginMetaData(plugin)).to.be.deep.eq({
-            name: 'Sample Plugin',
-            version: '1.0.0',
+            name: "Sample Plugin",
+            version: "1.0.0",
             requiresRootAccess: false,
-            iconUrl: '',
-            appUrl: ''
+            iconUrl: "",
+            appUrl: "",
         });
     });
 });
