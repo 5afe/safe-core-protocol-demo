c5b639457581fb6ce63acc0df2e0602d915847f4
Richard Meissner <rmeissner@users.noreply.github.com>
2023-07-13 23:03:36 +0200

    Add tasks to interact with test registry (#14)

---
+++ contracts/src/tasks/test_registry.ts
@@ -0,0 +1,27 @@
+import "hardhat-deploy";
+import "@nomicfoundation/hardhat-ethers";
+import { task } from "hardhat/config";
+import { getPlugin, getRegistry, getSamplePlugin } from "../utils/contracts";
+import { IntegrationType } from "../utils/constants";
+import { loadPluginMetaData } from "../utils/metadata";
+
+task("register-plugin", "Registers the sample Plugin in the Safe{Core} test register")
+    .setAction(async (_, hre) => {
+        const registry = await getRegistry(hre)
+        const plugin = await getSamplePlugin(hre)
+        await registry.addIntegration(await plugin.getAddress(), IntegrationType.Plugin)
+        console.log("Registered Plugin registry")
+    });
+
+task("list-plugins", "List available Plugins in the Safe{Core} test register")
+    .setAction(async (_, hre) => {
+        const registry = await getRegistry(hre)
+        const events = await registry.queryFilter(registry.filters.IntegrationAdded)
+        for (const event of events) {
+            const plugin = await getPlugin(hre, event.args.integration)
+            const metaData = await loadPluginMetaData(hre, plugin)
+            console.log(event.args.integration, metaData)
+        }
+    });
+
+export { }
