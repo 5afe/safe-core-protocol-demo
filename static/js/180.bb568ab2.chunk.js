c0086f1bf3f228ca0540025882731cfcda79cbf8
Richard Meissner <msc.meissner@gmail.com>
2023-07-16 01:07:14 +0200

    Updates

---
+++ static/js/180.bb568ab2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksafe_core_protocol_app=self.webpackChunksafe_core_protocol_app||[]).push([[180],{69180:function(e,o,t){t.r(o);var f=null;"undefined"!==typeof WebSocket?f=WebSocket:"undefined"!==typeof MozWebSocket?f=MozWebSocket:"undefined"!==typeof t.g?f=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?f=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(f=self.WebSocket||self.MozWebSocket),o.default=f}}]);
+//# sourceMappingURL=180.bb568ab2.chunk.js.map
