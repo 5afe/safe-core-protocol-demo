c37bf2da6ddc0a4be608bc430d709607ebf4a1a5
Mikhail <mveehkim@gmail.com>
2023-09-05 15:23:16 +0200

    Feature: ERC4337 plugin

---
+++ contracts/contracts/ERC4337Plugin.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.18;
+import {ISafe} from "@safe-global/safe-core-protocol/contracts/interfaces/Accounts.sol";
+import {ISafeProtocolPlugin} from "@safe-global/safe-core-protocol/contracts/interfaces/Integrations.sol";
+import {ISafeProtocolManager} from "@safe-global/safe-core-protocol/contracts/interfaces/Manager.sol";
+import {SafeTransaction, SafeRootAccess, SafeProtocolAction} from "@safe-global/safe-core-protocol/contracts/DataTypes.sol";
+import {BasePluginWithEventMetadata, PluginMetadata} from "./Base.sol";
+
+/**
+ * @title OwnerManager
+ * @dev This interface is defined for use in WhitelistPlugin contract.
+ */
+interface OwnerManager {
+    function isOwner(address owner) external view returns (bool);
+}
+
+/**
+ * @title WhitelistPlugin maintains a mapping that stores information about accounts that are
+ *        permitted to execute non-root transactions through a Safe account.
+ * @notice This plugin does not need Safe owner(s) confirmation(s) to execute Safe txs once enabled
+ *         through a Safe{Core} Protocol Manager.
+ */
+contract ERC4337Plugin is BasePluginWithEventMetadata {
+    constructor()
+        BasePluginWithEventMetadata(
+            PluginMetadata({name: "ERC4337 Plugin", version: "1.0.0", requiresRootAccess: false, iconUrl: "", appUrl: ""})
+        )
+    {}
+}
--- contracts/src/utils/protocol.ts
+++ contracts/src/utils/protocol.ts
@@ -1,8 +1,8 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import protocolDeployments from "@safe-global/safe-core-protocol"
+import protocolDeployments from "@safe-global/safe-core-protocol";
 import { id } from "ethers";
 
-const deployMock = async(hre: HardhatRuntimeEnvironment, name: string): Promise<string> => {
+const deployMock = async (hre: HardhatRuntimeEnvironment, name: string): Promise<string> => {
     const { deployments, getNamedAccounts } = hre;
     const { deployer } = await getNamedAccounts();
     const { deploy } = deployments;
@@ -13,30 +13,30 @@ const deployMock = async(hre: HardhatRuntimeEnvironment, name: string): Promise<
         log: true,
         deterministicDeployment: id(name),
     });
-    return result.address
-}
+    return result.address;
+};
 
-export const getProtocolManagerAddress = async(hre: HardhatRuntimeEnvironment): Promise<string> => {
-    const chainId = await hre.getChainId()
+export const getProtocolManagerAddress = async (hre: HardhatRuntimeEnvironment): Promise<string> => {
+    const chainId = await hre.getChainId();
 
     // For the tests we deploy a mock for the manager
-    if (chainId === "31337") return deployMock(hre, "ManagerMock")
-    
-    if (!(chainId in protocolDeployments)) throw Error("Unsupported Chain")
-    const manager = (protocolDeployments as any)[chainId][0].contracts.SafeProtocolManager.address
-    if (typeof manager !== "string") throw Error("Unexpected Manager")
-    return manager
-}
+    if (chainId === "31337") return deployMock(hre, "ManagerMock");
 
-export const getProtocolRegistryAddress = async(hre: HardhatRuntimeEnvironment): Promise<string> => {
-    const chainId = await hre.getChainId()
+    if (!(chainId in protocolDeployments)) throw Error("Unsupported Chain");
+    const manager = (protocolDeployments as any)[chainId][0].contracts.SafeProtocolManager.address;
+    if (typeof manager !== "string") throw Error("Unexpected Manager");
+    return manager;
+};
+
+export const getProtocolRegistryAddress = async (hre: HardhatRuntimeEnvironment): Promise<string> => {
+    const chainId = await hre.getChainId();
 
     // For the tests we deploy a mock for the registry
-    if (chainId === "31337") return deployMock(hre, "RegistryMock")
-    
-    if (!(chainId in protocolDeployments)) throw Error("Unsupported Chain")
+    if (chainId === "31337") return deployMock(hre, "RegistryMock");
+
+    if (!(chainId in protocolDeployments)) throw Error("Unsupported Chain");
     // We use the unrestricted registry for the demo
-    const registry = (protocolDeployments as any)[chainId][0].contracts.TestSafeProtocolRegistryUnrestricted.address
-    if (typeof registry !== "string") throw Error("Unexpected Registry")
-    return registry
-}
+    const registry = (protocolDeployments as any)[chainId][0].contracts.TestSafeProtocolRegistryUnrestricted.address;
+    if (typeof registry !== "string") throw Error("Unexpected Registry");
+    return registry;
+};
